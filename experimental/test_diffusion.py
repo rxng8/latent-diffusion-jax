19a9a9b2d32c22cdf086fdd543e018caac417c4b
Viet Nguyen <alexvn.edu@gmail.com>
2024-06-03 23:09:29 -0400

    refactoring report function

--- experimental/test_diffusion.py
+++ experimental/test_diffusion.py
@@ -23,75 +23,6 @@ transform = lambda x: x / 255.0 * 2 - 1
 untransform = lambda x: ((x / 2 + 0.5) * 255.0).astype(np.uint8)
 
 
-class NoiseEstimatorUNet(nj.Module):
-
-  stage: int = 3
-  head: int = 1
-  group: int = 1
-  thidden: int = 32
-  act: str = "gelu"
-
-  def __init__(self, hidden: int) -> None:
-    self._hidden = hidden
-
-  def __call__(self, imgnoi: jax.Array, timeid: jax.Array, condition: jax.Array=None) -> jax.Array:
-    # imgnoi (B, H, W, C), -> timeid: (B,) -> condition (B, H, W, C) or (B, S, C) -> noise (B, H, W, C)
-    """_summary_
-
-    Args:
-        imgnoi (jax.Array): (B, H, W, C)
-        timeid (jax.Array): (B,)
-        condition (jax.Array): (B, S, Z) or (B, H, W, Z)
-
-    Returns:
-        jax.Array: (B, Z1)
-    """
-    if condition is None:
-      condition = imgnoi
-    B, H, W, C = imgnoi.shape
-    x = self.get("conv", nn.Conv2D, self._hidden, 1)(imgnoi)
-    time_embed = self.get("time", nn.TimeEmbedding, self.thidden)(timeid)
-    carries = []
-
-    #### downsampling
-    dim = self._hidden * 2
-    for s in range(self.stage):
-      # two res blocks + self attention + add together
-      x = self.get(f"dr{s}0", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
-      x = self.get(f"dr{s}1", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
-      x = self.get(f"da{s}", nn.CrossAttentionBlock, dim, head=self.head, group=self.group)(x, condition)
-      carries.append(x)
-      # Downsampling
-      if s != self.stage - 1:
-        x = self.get(f"dd{s}", nn.Conv2D, dim, 3, stride=2)(x)
-      # increase dimension
-      dim *= 2
-
-    #### bottleneck
-    x = self.get(f"br0", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
-    x = self.get(f"ba", nn.CrossAttentionBlock, dim, head=self.head, group=self.group)(x, condition)
-    x = self.get(f"br1", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
-
-    #### upsampling
-    # Upsampling phase
-    dim //= 2
-    for s in reversed(range(self.stage)):
-      x = jnp.concatenate([carries.pop(), x], -1)
-      # two res blocks + self attention + add together
-      x = self.get(f"ur{s}0", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
-      x = self.get(f"ur{s}1", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
-      x = self.get(f"ua{s}", nn.CrossAttentionBlock, dim, head=self.head, group=self.group)(x, condition)
-      # Upsampling
-      if s != 0:
-        x = self.get(f"uu{s}", nn.Conv2D, dim, 3, stride=2, transp=True)(x)
-      # decrease dimension
-      dim //= 2
-
-    # Final ResNet block and output convolutional layer
-    x = self.get(f"fr", nn.ResidualTimeBlock, self._hidden, act=self.act, group=self.group)(x, time_embed)
-    x = self.get(f"out", nn.Conv2D, C, 1, stride=1)(x)
-    return x
-
 def test_noise_estimator_unet():
   N = NoiseEstimatorUNet(8, stage=5, head=8, group=8, name="U")
   B, H, W, C = 2, 64, 64, 3
@@ -106,110 +37,6 @@ def test_noise_estimator_unet():
   assert out.shape == (B, H, W, C)
 
 
-class Diffuser(nj.Module):
-
-  hidden: int = 8
-  stage: int = 3
-  head: int = 1
-  group: int = 1
-  thidden: int = 32
-  act: str = "gelu"
-
-  # implement the algorithm from https://arxiv.org/pdf/2006.11239.pdf
-  # adapt from: https://github.com/andylolu2/jax-diffusion/blob/main/jax_diffusion/diffusion.py
-  def __init__(self, beta_start: float, beta_final: float, steps: int):
-    """_summary_
-
-    Args:
-      beta_start (float): the beta/variance of x_0 or the observation/frame/image
-      beta_final (float): the beta/variance of x_T-1 or the latent/diffused noise
-      steps (int): the total number of steps in the mdp
-    """
-    self._betas = np.linspace(beta_start, beta_final, steps) # (T,)
-    self._alphas = 1 - self._betas # (T,)
-    self._alpha_bars = np.cumprod(self._alphas) # (T,)
-    self._steps = steps # ()
-    self.unet = NoiseEstimatorUNet(self.hidden, stage = self.stage, head = self.head,
-      group = self.group, thidden = self.thidden, act = self.act, name="unet")
-
-  def forward(self, x_0: jax.Array, t: jax.Array) -> jax.Array:
-    # given the image, add noise to it. See algorithm 1 in https://arxiv.org/pdf/2006.11239.pdf
-    # For forward, we can compute a random t
-    B, H, W, C = x_0.shape
-    (B,) = t.shape
-    """x_t, eps = self.sample_q(x_0, t): Samples x_t given x_0 by the q(x_t|x_0) formula."""
-    # x_0: (B, H, W, C)
-    alpha_bar_t = self._alpha_bars.take(t.astype(jnp.int32)) # (B,)
-    alpha_bar_t = alpha_bar_t[: None, None] # (B, 1, 1)
-    eps = jax.random.normal(nj.seed(), shape=x_0.shape, dtype=x_0.dtype)
-    x_t = jnp.sqrt(alpha_bar_t) * x_0 + jnp.sqrt(1 - alpha_bar_t) * eps
-    """end of x_t, eps = self.sample_q(x_0, t)"""
-    return x_t.clip(-1, 1), eps
-
-  def reverse_step(self, x_t: jax.Array, t: jax.Array):
-    """See algorithm 2 in https://arxiv.org/pdf/2006.11239.pdf"""
-    B, H, W, C = x_t.shape
-    (B,) = t.shape
-    alpha_t = jnp.take(self._alphas, t)
-    alpha_bar_t = jnp.take(self._alpha_bars, t)
-    sigma_t = jnp.sqrt(jnp.take(self._betas, t))
-    z = (t > 0) * jax.random.normal(nj.seed(), shape=x_t.shape, dtype=x_t.dtype)
-    eps = self.unet(x_t, t)
-    x = (1.0 / jnp.sqrt(alpha_t)) * (
-      x_t - ((1 - alpha_t) / jnp.sqrt(1 - alpha_bar_t)) * eps
-    ) + sigma_t * z
-    x = x.clip(-1, 1)
-    return x, x
-
-  def reverse(self, x_T: jax.Array) -> jax.Array:
-    B, H, W, C = x_T.shape
-    # given the noise, reconstruct the image
-    # For reverse, we have to reverse it one by one
-    ts = jnp.arange(0, self._steps)[..., None] # (T, 1)
-    ts = jnp.repeat(ts, B, axis=1) # (T, B)
-    x_hat_0, xs = nj.scan(self.reverse_step, x_T, ts, reverse=True, unroll=1, axis=0)
-    return x_hat_0, xs
-  
-
-class Trainer(nj.Module):
-  def __init__(self, config):
-    self.config = config
-    self.diffuser = Diffuser(**config.diffuser, name="diff")
-    self.opt = nn.Optimizer(**config.opt, name="opt")
-    self.modules = [self.diffuser]
-
-  def train(self, data):
-    opt_metrics, (outs, loss_metrics) = self.opt(self.modules,
-      self.loss, data, has_aux=True)
-    opt_metrics.update(loss_metrics)
-    return outs, opt_metrics
-
-  def infer(self, data):
-    return self.diffuser.reverse(data["image"])
-
-  def loss(self, data):
-    x_0 = data["image"]
-    B, H, W, C = x_0.shape
-
-    # Generate random timesteps indices
-    timesteps = np.random.randint(0, self.diffuser._steps, (B,))
-    timesteps = nn.cast(timesteps)
-
-    # Generating the noise and noisy image for this batch
-    # Add noise to x_0 until timestep
-    noisy_image, noise = self.diffuser.forward(x_0, timesteps)
-
-    # Forward noising: given a noisy image, predict the noise added to that image
-    pred_noise = self.diffuser.unet(noisy_image, timesteps)
-
-    # l1 loss
-    # loss = ((pred_noise - noise)**2).mean([-3, -2, -1]).mean()
-    loss = ((pred_noise - noise)**2).mean()
-
-    # metrics
-    outs = {"pred_noise": pred_noise, "noise": noise, "noisy_image": noisy_image}
-    metrics = {"loss": loss}
-    return loss, (outs, metrics)
 
 config = embodied.Config(
   diffuser=dict(
@@ -286,7 +113,7 @@ for i, tid in enumerate([299, 189, 150, 140, 90, 50, 10, 0]):
   plt.imshow(untransform(xs[tid][0].clip(-1, 1)))
   plt.axis('off')
   plt.title(f"t={tid}")
-plt.tight_layout
+plt.tight_layout()
 plt.show()
 
 
--- ldm/nets.py
+++ ldm/nets.py
@@ -1,93 +1,157 @@
-# """
-# File: nets.py
-# Author: Viet Nguyen
-# Date: 2024-06-01
+"""
+File: nets.py
+Author: Viet Nguyen
+Date: 2024-06-03
 
-# Description: Cycle GAN components
-# """
+Description: all networks logic stays here
+"""
 
-# import numpy as np
-# import jax
-# import jax.numpy as jnp
+import numpy as np
+import jax
+import jax.numpy as jnp
 
-# from functools import partial as bind
+from functools import partial as bind
 
-# import embodied
-# from embodied.nn import ninjax as nj
-# from embodied import nn
-# from embodied.nn import sg
-
-
-# class Generator(nj.Module):
-
-#   block: int = 2
-#   hidden: int = 16
-#   stage: int = 2
-
-#   def __init__(self, **kw) -> None:
-#     # NOTE the number of hidden is capped at 512
-#     self._kw = kw
-
-#   def __call__(self, inputs: jax.Array):
-#     # inputs: (B, H, W, C)
-#     B, H, W, C = inputs.shape
-#     # Initial convolution layers
-#     x = nn.reflection_pad_2d(inputs, 3)
-#     x = self.get("in", nn.Conv2D, self.hidden, 7, stride=1, pad='valid', **self._kw)(x)
-#     # encoding
-#     _hidden = self.hidden * 2
-#     for s in range(self.stage):
-#       x = self.get(f"ds{s}", nn.Conv2D, np.minimum(_hidden, 512), 3, stride=2, pad='same', **self._kw)(x)
-#       _hidden *= 2
-#     # residual blocks
-#     for b in range(self.block):
-#       x = self.get(f"b{b}", nn.ResidualBlock)(x)
-#     # upsampling, decoding
-#     _hidden //= 2
-#     for s in range(self.stage):
-#       x = self.get(f"us{s}", nn.Conv2D, np.minimum(_hidden, 512), 3, stride=2, transp=True, pad='same', **self._kw)(x)
-#     # conv out
-#     # Initial convolution layers
-#     kw = {**self._kw, 'act': 'tanh', 'norm': 'none'}
-#     x = nn.reflection_pad_2d(x, 3)
-#     x = self.get("out", nn.Conv2D, C, 7, stride=1, pad='valid', **kw)(x)
-#     # return
-#     return x
+import embodied
+from embodied.nn import ninjax as nj
+from embodied import nn
+from embodied.nn import sg
 
 
-# class Discriminator(nj.Module):
 
-#   stage: int = 2
-#   hidden: int = 16
-#   act: str = 'leaky_relu'
-#   norm: str = 'instance'
+class NoiseEstimatorUNet(nj.Module):
 
-#   def __init__(self, **kw) -> None:
-#     # NOTE: the number of hidden is capped at 512
-#     self._kw = kw
-#     unused_keys = ["act", "pad", "stride", "transp", "hidden", "norm"]
-#     for uk in unused_keys:
-#       self._kw.pop(uk, None)
+  stage: int = 3
+  head: int = 1
+  group: int = 1
+  thidden: int = 32
+  act: str = "gelu"
 
-#   def output_shape(self, shape: tuple):
-#     H, W = shape
-#     return (H // 2**self.stage, W // 2**self.stage)
+  def __init__(self, hidden: int) -> None:
+    self._hidden = hidden
 
-#   def __call__(self, inputs: jax.Array):
-#     # inputs: (B, H, W, C)
-#     B, H, W, C = inputs.shape
+  def __call__(self, imgnoi: jax.Array, timeid: jax.Array, condition: jax.Array=None) -> jax.Array:
+    # imgnoi (B, H, W, C), -> timeid: (B,) -> condition (B, H, W, C) or (B, S, C) -> noise (B, H, W, C)
+    """_summary_
 
-#     # input:
-#     x = self.get("in", nn.Conv2D, self.hidden, 3, pad='same', stride=2,
-#       norm='none', act=self.act, **self._kw)(inputs)
-#     # downblock
-#     _hidden = self.hidden * 2
-#     for s in range(self.stage - 1):
-#       x = self.get(f"s{s}", nn.Conv2D, np.minimum(_hidden, 512), 3, pad='same', stride=2,
-#         norm=self.norm, act=self.act, **self._kw)(x)
-#       _hidden *= 2
-#     # out
-#     x = self.get(f"out", nn.Conv2D, 1, 3, pad='same', stride=1, act='none')(x) # NOTE: TODO: do zeropadding for only the bottom right
-#     return x
+    Args:
+        imgnoi (jax.Array): (B, H, W, C)
+        timeid (jax.Array): (B,)
+        condition (jax.Array): (B, S, Z) or (B, H, W, Z)
 
+    Returns:
+        jax.Array: (B, Z1)
+    """
+    if condition is None:
+      condition = imgnoi
+    B, H, W, C = imgnoi.shape
+    x = self.get("conv", nn.Conv2D, self._hidden, 1)(imgnoi)
+    time_embed = self.get("time", nn.TimeEmbedding, self.thidden)(timeid)
+    carries = []
+
+    #### downsampling
+    dim = self._hidden * 2
+    for s in range(self.stage):
+      # two res blocks + self attention + add together
+      x = self.get(f"dr{s}0", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
+      x = self.get(f"dr{s}1", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
+      x = self.get(f"da{s}", nn.CrossAttentionBlock, dim, head=self.head, group=self.group)(x, condition)
+      carries.append(x)
+      # Downsampling
+      if s != self.stage - 1:
+        x = self.get(f"dd{s}", nn.Conv2D, dim, 3, stride=2)(x)
+      # increase dimension
+      dim *= 2
+
+    #### bottleneck
+    x = self.get(f"br0", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
+    x = self.get(f"ba", nn.CrossAttentionBlock, dim, head=self.head, group=self.group)(x, condition)
+    x = self.get(f"br1", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
+
+    #### upsampling
+    # Upsampling phase
+    dim //= 2
+    for s in reversed(range(self.stage)):
+      x = jnp.concatenate([carries.pop(), x], -1)
+      # two res blocks + self attention + add together
+      x = self.get(f"ur{s}0", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
+      x = self.get(f"ur{s}1", nn.ResidualTimeBlock, dim, act=self.act, group=self.group)(x, time_embed)
+      x = self.get(f"ua{s}", nn.CrossAttentionBlock, dim, head=self.head, group=self.group)(x, condition)
+      # Upsampling
+      if s != 0:
+        x = self.get(f"uu{s}", nn.Conv2D, dim, 3, stride=2, transp=True)(x)
+      # decrease dimension
+      dim //= 2
+
+    # Final ResNet block and output convolutional layer
+    x = self.get(f"fr", nn.ResidualTimeBlock, self._hidden, act=self.act, group=self.group)(x, time_embed)
+    x = self.get(f"out", nn.Conv2D, C, 1, stride=1)(x)
+    return x
+
+
+
+class Diffuser(nj.Module):
+
+  hidden: int = 8
+  stage: int = 3
+  head: int = 1
+  group: int = 1
+  thidden: int = 32
+  act: str = "gelu"
+
+  # implement the algorithm from https://arxiv.org/pdf/2006.11239.pdf
+  # adapt from: https://github.com/andylolu2/jax-diffusion/blob/main/jax_diffusion/diffusion.py
+  def __init__(self, beta_start: float, beta_final: float, steps: int):
+    """_summary_
+
+    Args:
+      beta_start (float): the beta/variance of x_0 or the observation/frame/image
+      beta_final (float): the beta/variance of x_T-1 or the latent/diffused noise
+      steps (int): the total number of steps in the mdp
+    """
+    self._betas = np.linspace(beta_start, beta_final, steps) # (T,)
+    self._alphas = 1 - self._betas # (T,)
+    self._alpha_bars = np.cumprod(self._alphas) # (T,)
+    self._steps = steps # ()
+    self.unet = NoiseEstimatorUNet(self.hidden, stage = self.stage, head = self.head,
+      group = self.group, thidden = self.thidden, act = self.act, name="unet")
+
+  def forward(self, x_0: jax.Array, t: jax.Array) -> jax.Array:
+    # given the image, add noise to it. See algorithm 1 in https://arxiv.org/pdf/2006.11239.pdf
+    # For forward, we can compute a random t
+    B, H, W, C = x_0.shape
+    (B,) = t.shape
+    """x_t, eps = self.sample_q(x_0, t): Samples x_t given x_0 by the q(x_t|x_0) formula."""
+    # x_0: (B, H, W, C)
+    alpha_bar_t = self._alpha_bars.take(t.astype(jnp.int32)) # (B,)
+    alpha_bar_t = alpha_bar_t[: None, None] # (B, 1, 1)
+    eps = jax.random.normal(nj.seed(), shape=x_0.shape, dtype=x_0.dtype)
+    x_t = jnp.sqrt(alpha_bar_t) * x_0 + jnp.sqrt(1 - alpha_bar_t) * eps
+    """end of x_t, eps = self.sample_q(x_0, t)"""
+    return x_t.clip(-1, 1), eps
+
+  def reverse_step(self, x_t: jax.Array, t: jax.Array):
+    """See algorithm 2 in https://arxiv.org/pdf/2006.11239.pdf"""
+    B, H, W, C = x_t.shape
+    (B,) = t.shape
+    alpha_t = jnp.take(self._alphas, t)
+    alpha_bar_t = jnp.take(self._alpha_bars, t)
+    sigma_t = jnp.sqrt(jnp.take(self._betas, t))
+    z = (t > 0) * jax.random.normal(nj.seed(), shape=x_t.shape, dtype=x_t.dtype)
+    eps = self.unet(x_t, t)
+    x = (1.0 / jnp.sqrt(alpha_t)) * (
+      x_t - ((1 - alpha_t) / jnp.sqrt(1 - alpha_bar_t)) * eps
+    ) + sigma_t * z
+    x = x.clip(-1, 1)
+    return x, x
+
+  def reverse(self, x_T: jax.Array) -> jax.Array:
+    B, H, W, C = x_T.shape
+    # given the noise, reconstruct the image
+    # For reverse, we have to reverse it one by one
+    ts = jnp.arange(0, self._steps)[..., None] # (T, 1)
+    ts = jnp.repeat(ts, B, axis=1) # (T, B)
+    x_hat_0, xs = nj.scan(self.reverse_step, x_T, ts, reverse=True, unroll=1, axis=0)
+    return x_hat_0, xs
+  
 
